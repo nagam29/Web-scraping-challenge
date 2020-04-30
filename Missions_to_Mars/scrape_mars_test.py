448be3293510771fadcaf772cd19f371eec85edc
nagam29 <mika.nagamine@gmail.com>
2020-04-30 01:09:08 -0700

    4/29/2020 update

--- Missions_to_Mars/scrape_mars_test.py
+++ Missions_to_Mars/scrape_mars_test.py
@@ -51,21 +51,150 @@ def scrape():
 
     # Mars Weather
     url = 'https://twitter.com/marswxreport?lang=en'
-    #url = 'https://twitter.com/MarsWxReport/status/1254875768108331008'
     browser.visit(url)
+    import time
+    time.sleep(10)
     html = browser.html
     soup = bs(html, 'html.parser')
     ### print(soup.prettify())
     # text of mars weather is between span tags. So, just find all the text related to span tags
-    soup2 = bs(html)
-    for span_tag in soup2.find_all('span'):
-        print(span_tag.text)
-        mars_weather_temp=span_tag.text
-        # there are a few mars weathers are posted. Let's save one
-        mars_weather='InSight sol 503 (2020-04-26) low -93.8ºC (-136.8ºF) high -4.9ºC (23.2ºF) winds from the WNW at 4.6 m/s (10.2 mph) gusting to 17.5 m/s (39.1 mph) pressure at 6.70 hPa'
-        print(mars_weather)
+    # text of mars weather is between span tags. So, just find all the text related to span tags
+    #soup2 = bs(html)
+    weather_report=[]
+    for span_tag in soup.find_all('span'):
+        if 'sol' in span_tag.text:
+            weather_report.append(span_tag.text) 
+        print("Found span text:", span_tag.text)
+    ### print(weather_report[0])
 
+    # there are a few mars weathers are posted. Let's save one
+    mars_weather=weather_report[0]
+    ### print(mars_weather)
     scraped["weather"] = mars_weather
+
+    # Mars Facts
+    url = 'https://space-facts.com/mars/'
+    tables = pd.read_html(url)
+    ### tables
+    # Keep the first table in data frame
+    df=tables[0]
+    ### print(df)
+    df2=df.iloc[:]
+    ### df2
+    df2.columns = ['description','value']
+    # set description as index
+    df2.set_index('description', inplace=True)
+    ### df2
+    # Convert it to html
+    html_table = df2.to_html()
+    ### html_table
+    # Remove \n
+    mars_fact_html=html_table.replace('\n', '')
+    ### mars_fact_html
+    scraped["fact"] = mars_fact_html
+    
+    # Mars Hemispheres
+
+    # Cerberus Hemisphere
+    url_cer = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced'
+    browser.visit(url_cer)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    ### print(soup.prettify())
+
+    for result in soup:
+        try:
+            title=result.find('h2', class_="title").text
+            link=result.find_all('a',href="http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg")
+        
+            ###if (title and link):
+                ### print(title)
+                ### print(link)
+        except AttributeError as e:
+            print(e)
+
+        title_cer=title
+        link_cer=link
+        print(title_cer,link_cer)
+
+    # Schiaparelli Hemisphere
+
+    url_shi = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced'
+    browser.visit(url_shi)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    ### print(soup.prettify())
+    for result in soup:
+        try:
+            title=result.find('h2', class_="title").text
+            link=result.find_all('a',href="http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg")
+            
+            ### if (title and link):
+                ### print(title)
+                ### print(link)
+        except AttributeError as e:
+            print(e)
+
+        title_schi=title
+        link_schi=link
+        ### print(title_schi,link_schi)
+
+    # Syrtis Major Hemisphere
+
+    url_syr = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced'
+    browser.visit(url_syr)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    ### print(soup.prettify())
+
+    for result in soup:
+        try:
+            title=result.find('h2', class_="title").text
+            link=result.find_all('a',href="http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg")
+            
+           ###  if (title and link):
+                ### print(title)
+                ### print(link)
+        except AttributeError as e:
+            print(e)
+
+        title_syr=title
+        link_syr=link
+        ### print(title_syr,link_syr)
+
+    # Valles Marineris Hemisphere 
+    url_val = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced'
+    browser.visit(url_val)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    ### print(soup.prettify())
+
+    for result in soup:
+        try:
+            title=result.find('h2', class_="title").text
+            link=result.find_all('a',href="http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg")
+            
+            ### if (title and link):
+                ### print(title)
+                ### print(link)
+        except AttributeError as e:
+            print(e)
+
+        title_val=title
+        link_val=link
+        ### print(title_val,link_val)
+
+    hemisphere_image_urls = [
+        {"title": "Cerberus Hemisphere Enhanced", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg"},
+        {"title": "Schiaparelli Hemisphere Enhanced", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg"},
+        {"title": "Syrtis Major Hemisphere Enhanced", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg"},
+        {"title": "Valles Marineris Hemisphere Enhanced", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg"},
+    ]
+    print(hemisphere_image_urls)
 
     return scraped
 
